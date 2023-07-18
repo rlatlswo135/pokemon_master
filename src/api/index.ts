dd784da93dfe87be98b975ba16efdeea1738c96a
rlatlswo13 <rlatlswo13@onpia.co.kr>
2023-07-18 15:36:37 +0900

    chore: test

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,5 +2,6 @@
 https://pokeapi.co/api/v2/generation/1 -> 1세대
 */
 
+console.log('test');
 export const getOneGenerationPokemon = async () =>
     (await fetch('https://pokeapi.co/api/v2/generation/1')).json();
